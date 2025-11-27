42c60476f663c428760a607280113b0079817db9
Sandip Tamang <136832190+SandipTamangDev@users.noreply.github.com>
2025-11-27 20:56:30 +0545

    chore: scaffold layout with HTML/CSS and add JS structure with guiding comments

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+/*
+there are colors from 1 to f and there are total of 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F so what i can do have ONE array with those as elements and in it and randomly choose one and run it six time each getting a color for then show it to the box of the html.
+
+how i should do it
+- make an array with the hex codes.
+- 
+
+Total Tools Needed (one clean list)
+Array literal 
+.length
+Array.from()
+.join()
+Math.random()
+Math.floor()
+document.getElementById() / document.querySelector()
+.style.backgroundColor
+.innerText / .textContent
+*/
+
+const colorCode = [1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F];
+
+const colorGenerateBtn = document.getElementById('color-generator');
+const colorBox = document.querySelector('color-box');
+
+colorGenerateBtn.addEventListener('click', () => {
+    
+});
+
+
+
+
+
+
